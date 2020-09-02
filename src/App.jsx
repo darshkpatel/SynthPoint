dfa6e00b760d6ec9fef46f8b8945ac9c352a191d
Darsh <darshkpatel@gmail.com>
2020-09-02 17:37:23 +0530

    add tonejs tones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import * as Tone from 'tone';
 import Button from './components/Button';
 
 function App() {
   const isMobile = (window.screen.width < 780);
+  const synth = new Tone.Synth();
 
+  // Set wave type
+  synth.oscillator.type = 'sine';
+
+  document.addEventListener(
+    'mousedown', () => {
+      if (Tone.context.state !== 'running') {
+        Tone.context.resume();
+      }
+    },
+  );
+
+  // Connect to master output
+  synth.toDestination();
+  useEffect(() => {
+    if (isMobile) {
+      console.log('Adding Listners');
+      const notes = document.getElementById('notes');
+      // Event Listener for clicking "on" notes
+      notes.addEventListener('mousedown', (e) => {
+      // Grabs note name from 'data-note'
+        try {
+          console.log('Triggering', e.target.innerText);
+          synth.triggerAttack(e.target.innerText, '16n');
+        } catch (e) {
+          console.log(e);
+        }
+      });
+
+      // Event Listener for clicking "off" notes
+      notes.addEventListener('mouseup', () => {
+        synth.triggerRelease();
+      });
+    }
+  }, [synth]);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,15 +48,15 @@ function App() {
       </header>
       {isMobile
       && (
-      <div className="container">
-        <Button><span>A</span></Button>
-        <Button><span>B</span></Button>
-        <Button><span>C</span></Button>
-        <Button><span>D</span></Button>
-        <Button><span>E</span></Button>
-        <Button><span>F</span></Button>
-        <Button><span>G</span></Button>
-        <Button><span>B</span></Button>
+      <div className="container" id="notes">
+        <Button><span>A#4</span></Button>
+        <Button><span>B#4</span></Button>
+        <Button><span>C#4</span></Button>
+        <Button><span>D#4</span></Button>
+        <Button><span>E#4</span></Button>
+        <Button><span>F#4</span></Button>
+        <Button><span>G#4</span></Button>
+        <Button><span>B#2</span></Button>
       </div>
       )}
       {!isMobile && (

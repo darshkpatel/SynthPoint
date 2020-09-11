dc507fe6283aac871610c88549909aab33600b9c
devamtrivedi <devamtrivedi71@gmail.com>
2020-09-11 11:40:48 +0530

    feat: added orientation sensor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React, { useEffect } from 'react';
 import './App.css';
 import * as Tone from 'tone';
 import Button from './components/Button';
+import {
+  AbsoluteOrientationSensor,
+  // RelativeOrientationSensor,
+} from 'motion-sensors-polyfill';
 
 function App() {
   const isMobile = (window.screen.width < 780);
@@ -18,6 +22,7 @@ function App() {
     },
   );
 
+
   // Connect to master output
   synth.toDestination();
   useEffect(() => {
@@ -26,7 +31,7 @@ function App() {
       const notes = document.getElementById('notes');
       // Event Listener for clicking "on" notes
       notes.addEventListener('mousedown', (e) => {
-      // Grabs note name from 'data-note'
+        // Grabs note name from 'data-note'
         try {
           console.log('Triggering', e.target.innerText);
           synth.triggerAttack(e.target.innerText, '16n');
@@ -40,6 +45,17 @@ function App() {
         synth.triggerRelease();
       });
     }
+
+    const sensor = new AbsoluteOrientationSensor();
+    const mat4 = new Float32Array(16);
+    sensor.start();
+    sensor.onerror = event => console.log(event.error.name, event.error.message);
+
+    sensor.onreading = () => {
+      sensor.populateMatrix(mat4);
+      // console.log(mat4)
+    }
+    console.log(mat4)
   }, [synth]);
   return (
     <div className="App">
@@ -47,22 +63,22 @@ function App() {
         <title>SynthPoint</title>
       </header>
       {isMobile
-      && (
-      <div className="container" id="notes">
-        <Button><span>A#4</span></Button>
-        <Button><span>B#4</span></Button>
-        <Button><span>C#4</span></Button>
-        <Button><span>D#4</span></Button>
-        <Button><span>E#4</span></Button>
-        <Button><span>F#4</span></Button>
-        <Button><span>G#4</span></Button>
-        <Button><span>B#2</span></Button>
-      </div>
-      )}
+        && (
+          <div className="container" id="notes">
+            <Button><span>A#4</span></Button>
+            <Button><span>B#4</span></Button>
+            <Button><span>C#4</span></Button>
+            <Button><span>D#4</span></Button>
+            <Button><span>E#4</span></Button>
+            <Button><span>F#4</span></Button>
+            <Button><span>G#4</span></Button>
+            <Button><span>B#2</span></Button>
+          </div>
+        )}
       {!isMobile && (
-      <div className="titleText">
-        <h1>Sorry, this app is only supported on mobile devices.</h1>
-      </div>
+        <div className="titleText">
+          <h1>Sorry, this app is only supported on mobile devices.</h1>
+        </div>
       )}
     </div>
   );
